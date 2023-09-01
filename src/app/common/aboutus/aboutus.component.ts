557701f0e89b5bb85c55a95b37115084ac9404bb
Sabarimani Ramamoorthi <86560376+Sabariramamoorthy@users.noreply.github.com>
2023-09-01 23:56:38 +0530

    optimizse

--- src/app/common/aboutus/aboutus.component.ts
+++ src/app/common/aboutus/aboutus.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { aboutUs } from 'src/Config/Constant';
 
 @Component({
@@ -7,6 +7,7 @@ import { aboutUs } from 'src/Config/Constant';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent {
+  @Input()  ishome :boolean=false;
   about !:any
   constructor(){
     this.about=aboutUs
